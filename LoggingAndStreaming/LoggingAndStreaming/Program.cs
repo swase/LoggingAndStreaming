ca6c675b42cc6201bae3cbc67c396d0825692d8b
swase <fg2g15@soton.ac.uk>
2021-08-13 14:48:23 +0100

    Update

--- LoggingAndStreaming/LoggingAndStreaming/Program.cs
+++ LoggingAndStreaming/LoggingAndStreaming/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//#define TEST
+using System;
+using System.IO;
+using System.Diagnostics;
 
 namespace LoggingAndStreaming
 {
@@ -6,7 +9,71 @@ namespace LoggingAndStreaming
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string text = "Hello out there";
+            //File.WriteAllText("Hello.txt", text);
+            //string readtext = File.ReadAllText("Hello.txt");
+            //Console.WriteLine(readtext);
+            //File.AppendAllText("Hello.txt", text);
+
+            //string fileName = "lines.txt";
+
+            //string[] lines =
+            //{
+            //    "Line 1",
+            //    "Line 2",
+            //    "Line 3"
+            //};
+            //File.WriteAllLines(fileName, lines);
+            //string[] readLines = File.ReadAllLines(fileName);
+            //foreach(var str in readLines)
+            //{
+            //    Console.WriteLine(str);
+            //}
+
+            //Debug.WriteLine("Debug - this is a debug item");
+            //Trace.WriteLine("Trace - This is a trace item");
+
+            //Console.WriteLine($"This is being logged at time {DateTime.Now}");
+            //TextWriterTraceListener twt1 = new TextWriterTraceListener(File.Create("TraceOutput.txt"));
+            //Trace.Listeners.Add(twt1);
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine(i);
+            //    Debug.WriteLine($"Debug - the value of i i is {i}");
+            //    Trace.WriteLine($"THe value of i is {i}");
+            //    Debug.WriteLineIf(i == 2, $"\nReached  maximum value of i: {i} at {DateTime.Now}");
+            //    Trace.Assert(i > 0, $"i is not grater than zero {i}");
+            //}
+            //Trace.Flush();
+            //#if DEBUG
+            //Console.WriteLine("This is Debug mode");
+            //#endif
+
+
+            //#if TEST
+            //Console.WriteLine("This is a test");
+            //#endif
+
+            string path = @"LiveForever.txt";
+
+            using(StreamWriter sr = File.AppendText(path))
+            {
+                sr.WriteLine("Live - Forever");
+                sr.Close();
+            }
+
+            using(StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+
+                }
+                sr.Close();
+
+            }
+
         }
     }
 }
