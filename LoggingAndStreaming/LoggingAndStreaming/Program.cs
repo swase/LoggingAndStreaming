16ee6c7141f9c8c7949acca1c8deab0af2073953
swase <fg2g15@soton.ac.uk>
2021-08-13 10:22:32 +0100

    Initial application creation

---
+++ LoggingAndStreaming/LoggingAndStreaming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggingAndStreaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
